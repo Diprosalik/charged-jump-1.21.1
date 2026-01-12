bff2b232b19eda60ce916355f97b6722da143882
Diprosalik <fabio.hofstadlergaming@gmail.com>
2026-01-12 21:27:11 +0100

    added particles on charging jump

--- src/main/java/net/diprosalik/chargejump/ChargeJumpClient.java
+++ src/main/java/net/diprosalik/chargejump/ChargeJumpClient.java
@@ -4,7 +4,11 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+import java.util.Objects;
 
 public class ChargeJumpClient implements ClientModInitializer {
     private boolean wasOnGround = false;
@@ -33,6 +37,9 @@ public class ChargeJumpClient implements ClientModInitializer {
                     && player.isSprinting()) {
 
                 readyForLongJump(player, SPEED);
+                if (player.age % 2 == 0) {
+                    spawnSprintParticles(player);
+                }
             }
 
             if (!onGround
@@ -64,7 +71,24 @@ public class ChargeJumpClient implements ClientModInitializer {
     }
 
     private void readyForLongJump(PlayerEntity player, double SPEED) {
-        player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED)
+        Objects.requireNonNull(player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED))
                 .setBaseValue(SPEED);
+    }
+
+    private static void spawnSprintParticles(PlayerEntity player) {
+        World world = player.getWorld();
+        Vec3d pos = player.getPos();
+
+        double spread = 0.5;
+
+        world.addParticle(
+                ParticleTypes.TRIAL_SPAWNER_DETECTION,
+                pos.x + (world.random.nextDouble() - 0.5) * spread,
+                pos.y + 0.05,
+                pos.z + (world.random.nextDouble() - 0.5) * spread,
+                0.0,
+                0.0,
+                0.0
+        );
     }
 }
