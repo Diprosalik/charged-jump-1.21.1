ae59e93f56fbda93599cd7d5354ad7599c24bdac
FabioHofstadler <230008@o365.htl-leoben.at>
2026-01-12 12:15:18 +0100

    Initial commit

---
+++ src/main/java/net/diprosalik/chargejump/ChargeJumpDataGenerator.java
@@ -0,0 +1,11 @@
+package net.diprosalik.chargejump;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChargeJumpDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
